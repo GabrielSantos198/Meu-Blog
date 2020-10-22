92a513d8dda9f8fd78968f3d881e6b9e71b2cbc6
gabriel <santosgabriel6763@gmail.com>
2020-10-22 09:18:30 -0300

    Upgrade2

--- son_blog/urls.py
+++ son_blog/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')), # new
 	path('ckeditor/', include('ckeditor_uploader.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
